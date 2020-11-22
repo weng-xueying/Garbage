9ae83aedc99781e4b8be14beba2b0da66942f10c
weng-xueying <1344024854@qq.com>
2020-11-22 23:32:29 +0800

    wxy
    wx-test

---
+++ pages/home/home.js
@@ -0,0 +1,81 @@
+// home/home.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    indicatorDots: true,//是否显示面板指示点
+    autoplay:false,//是否自动切换
+    interval:2000,//自动切换时间间隔
+    duration:300,//滑动动画时长
+    banner:[
+      {url:'../../image/1.jpg'},
+      {url:'../../image/2.jpeg'},
+      {url:'../../image/3.jpeg'}
+    ]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    wx.getSystemInfo({
+      success: function(res) {
+        that.setData({
+          height: res.statusBarHeight
+        })
+      },
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
