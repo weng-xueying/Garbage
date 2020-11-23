bbdbe03c5f44c65371f67136e3b1677e0d642003
weng-xueying <1344024854@qq.com>
2020-11-24 01:27:13 +0800

    wxy
    add searchbar

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,33 +1,18 @@
-// home/home.js
+// pages/search/search.js
 Page({
+
   /**
    * 页面的初始数据
    */
   data: {
-    indicatorDots: true,//是否显示面板指示点
-    autoplay:false,//是否自动切换
-    interval:2000,//自动切换时间间隔
-    duration:300,//滑动动画时长
-    true:true,
-    banner:[
-      {url:'../../image/1.jpg'},
-      {url:'../../image/2.jpeg'},
-      {url:'../../image/3.jpeg'}
-    ]
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that = this;
-    wx.getSystemInfo({
-      success: function(res) {
-        that.setData({
-          height: res.statusBarHeight
-        })
-      },
-    })
+
   },
 
   /**
